d129d43f6fdabcea1cfd8fb7d16203fe011ef991
Basedonsearch-Dindayal <dindayalsinha301@gmail.com>
2025-05-11 23:38:54 +0530

    Make simple lottery game in react

---
+++ lottery/lottery/src/Lottery.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react"
+import "./Lottery.css"
+import { generateTicket,sum} from "./helper";
+
+
+export default function Lottery() {
+    let [ticket,setTicket] = useState(generateTicket(3));
+    let isWinning = sum(ticket) === 15;
+    let buyTicket = () => {
+        setTicket(generateTicket(3))
+    }
+    return(
+        <div>
+            <h1>Lottery Game!</h1>
+            <div className="ticket">
+                <span>{ticket[0]}</span>
+                <span>{ticket[1]}</span>
+                <span>{ticket[2]}</span>
+            </div>
+            <div>
+                <br />
+                <button onClick={buyTicket}>Buy Ticket</button>
+            </div>
+            <h3>{isWinning && "Congratulation, you Won!"}</h3>
+        </div>
+    )
+}
